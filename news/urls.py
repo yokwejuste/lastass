379fac2edef87a40a95e13d2e1486dcc91c42b4e
spykelion <ndilionel6@gmail.com>
2021-06-17 08:50:14 +0100

    extended url config in news

--- news/urls.py
+++ news/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'news'
+
 urlpatterns = [
     url('', views.index, name='index'),
+    url('news/contact', views.contact, name='contact')
 ]
--- news/views.py
+++ news/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+# from django.forms import Forms
+
+# class Contact(Forms):
+#     name = ''
 
 def index(request):
     return render(request, 'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
