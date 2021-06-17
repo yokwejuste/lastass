807e3d5fc4ec9afb75c02cd4c36c61db3523f852
spykelion <ndilionel6@gmail.com>
2021-06-17 21:51:19 +0100

    added more views added login template updated the Post model

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,3 @@
-
-
 from django.urls import path
 
 from . import views
@@ -8,5 +6,7 @@ app_name = 'news'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('news/contact', views.contact, name='contact')
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login')
 ]
--- news/views.py
+++ news/views.py
@@ -7,5 +7,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
+def about(request):
+    return render(request, 'about.html')
+
 def contact(request):
     return render(request, 'contact.html')
+
+def login(request):
+    return render(request, 'login.html')
