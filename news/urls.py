71cf92c51b9cde9d08067b1750457ef21c1d3947
spykelion <ndilionel6@gmail.com>
2021-06-18 08:11:30 +0100

    login and logout logic

--- news/urls.py
+++ news/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('login/', views.login, name='login')
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
+    path('success/', views.success, name='success'),
+    path('post/create', views.create, name='create')
 ]
